3b8668c90e8f851f552dfc1275ed1adc4adb027e
Diego Righi <dev.drighi@icloud.com>
2025-07-20 02:45:15 -0300

    Stable commit

---
+++ CrewChange/src/main/java/br/com/drighi/crewchange/CrewChangeApplication.java
@@ -0,0 +1,13 @@
+package br.com.drighi.crewchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewChangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewChangeApplication.class, args);
+    }
+
+}
---
+++ CrewChange/src/test/java/br/com/drighi/crewchange/CrewChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drighi.crewchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewChangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
